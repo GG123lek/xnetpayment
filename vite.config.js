e7a2c53ace8fbb20a2054b1cfb1946c87707db16
Gafar <ridw5613@gmail.com>
2025-10-20 13:00:14 +0100

    fix my issue properly

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: '/',
   build: {
     outDir: 'dist',   // 👈 make sure your build output goes here
   },
